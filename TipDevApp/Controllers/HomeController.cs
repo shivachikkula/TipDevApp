b2dda3099ce36faa1e28f35fa0e46fa856b515da
shivachikkula <shivachikkula@gmail.com>
2020-10-02 14:59:10 -0400

    Commit

---
+++ TipDevApp/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+namespace TipDevApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            DataTable dt = new StudentRepo().GetStudentData();
+            return View(dt);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ TipDevApp/StudentRepo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace TipDevApp
+{
+    public class StudentRepo
+    {
+        public DataTable GetStudentData()
+        {
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+            SqlDataAdapter sqda = new SqlDataAdapter();
+            using (SqlConnection sqlcon = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["DHWDBConnection"].ConnectionString))
+            {
+                sqda.SelectCommand = new SqlCommand("SELECT * FROM dbo.Student",sqlcon);
+                sqda.Fill(ds);
+            }
+            return ds.Tables[0];
+        }
+
+    }
+}
